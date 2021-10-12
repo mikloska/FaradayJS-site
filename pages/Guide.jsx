148285f61ebd81ee90ddf34036cfecebedc543b8
mikloska <mikloska973@gmail.com>
2021-10-11 21:59:57 -0400

    Added skeleton of Guide page

--- pages/Guide.jsx
+++ pages/Guide.jsx
@@ -17,6 +17,23 @@ export default function Guide(){
         <h1 className={styles.title}>
           How to Use FaradayJS
         </h1>
+        {/* Change flex direction to column when screen is resized */}
+        <Flex direction={['column','column','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
+          <Flex  m={5} direction={['column','column','row','row']} justify='center' align='center'>
+            <Box m={5} mx={2} bg='red' width={100} height={100}>1</Box>         
+          </Flex>
+          <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
+            <Box m={5} mx={2} bg='pink' width={100} height={100}>2</Box>
+          </Flex>
+        </Flex>
+        <Flex direction={['column','column','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
+          <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
+            <Box m={5} mx={2} bg='green' width={100} height={100}>3</Box>
+          </Flex>
+          <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
+            <Box m={5} mx={2} bg='black' width={100} height={100}>4</Box>
+          </Flex>
+        </Flex>
 
   
       </main>
--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,10 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        {/* Change flex direction to column when screen is resized */}
         <Flex direction={['column','column','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
           <Flex  m={5} direction={['column','column','row','row']} justify='center' align='center'>
-            <Box m={5} mx={2} bg='red' width={100} height={100}>1</Box>
-                       
+            <Box m={5} mx={2} bg='red' width={100} height={100}>1</Box>         
           </Flex>
           <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
             <Box m={5} mx={2} bg='pink' width={100} height={100}>2</Box>
@@ -38,7 +38,6 @@ export default function Home() {
           <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
             <Box m={5} mx={2} bg='black' width={100} height={100}>4</Box>
           </Flex>
-
         </Flex>
 
   
