b9057245dc24b1ae523002ea7844f86bf6ce2628
mikloska <mikloska973@gmail.com>
2021-10-11 21:52:35 -0400

    Resolved resizing issues on homepage

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../public/components/Navbar'
 import Footer from '../public/components/Footer'
+import { Box,Flex } from "@chakra-ui/react"
 
 
 export default function Home() {
@@ -21,6 +22,24 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <Flex direction={['column','column','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
+          <Flex  m={5} direction={['column','column','row','row']} justify='center' align='center'>
+            <Box m={5} mx={2} bg='red' width={100} height={100}>1</Box>
+                       
+          </Flex>
+          <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
+            <Box m={5} mx={2} bg='pink' width={100} height={100}>2</Box>
+          </Flex>
+        </Flex>
+        <Flex direction={['column','column','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
+          <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
+            <Box m={5} mx={2} bg='green' width={100} height={100}>3</Box>
+          </Flex>
+          <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
+            <Box m={5} mx={2} bg='black' width={100} height={100}>4</Box>
+          </Flex>
+
+        </Flex>
 
   
       </main>
