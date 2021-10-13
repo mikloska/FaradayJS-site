c612479af9902d64e987b52a8164d3a6b5573f25
mikloska <mikloska973@gmail.com>
2021-10-12 22:00:57 -0400

    Removed download page and added download link to navbar. Changed how to use page to stack from Flex

--- pages/Guide.jsx
+++ pages/Guide.jsx
@@ -2,7 +2,7 @@ import Navbar from '../public/components/Navbar'
 import Footer from '../public/components/Footer'
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
-import { Box,Flex } from "@chakra-ui/react"
+import { Box,Flex, Stack } from "@chakra-ui/react"
 import Image from 'next/image'
 
 export default function Guide(){
@@ -20,14 +20,14 @@ export default function Guide(){
           How to Use FaradayJS
         </h1>
         {/* Change flex direction to column when screen is resized */}
-        <Flex direction={['column','column','row','row']} w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
+        <Stack direction={['column-reverse','column-reverse','row','row']} w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
           <Flex  m={5} direction={['column','column','row','row']} justify='center' align='center'>
-            <div>First enter the extensions of the files you would like to scan or don't select any to run on entire project. Click Run Tests.</div>         
+            <div>First enter the extensions of the files you would like to scan or leave it blank to run on entire project. Click Run Tests.</div>         
           </Flex>
           <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
             <img src='/Clipboard02.jpg' width={'100%'} height={'100%'}/>
           </Flex>
-        </Flex>
+        </Stack>
         <Flex direction={['column','column','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
           <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
           <img src='/Clipboard03.jpg' width={'100%'} height={'100%'}/>
@@ -37,14 +37,14 @@ export default function Guide(){
           </Flex>
         </Flex>
 
-        <Flex direction={['column','column','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
+        <Stack direction={['column-reverse','column-reverse','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
           <Flex  m={5} direction={['column','column','row','row']} justify='center' align='center'>
             <div>View your test results and fix errors.</div>         
           </Flex>
           <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
             <img src='/Clipboard04.jpg' width={'100%'} height={'100%'}/>
           </Flex>
-        </Flex>
+        </Stack>
         <Flex direction={['column','column','row','row']} maxW='1000px' w={['90vw','90vw','90vw','70vw']} justify='center' p="4">
           <Flex m={5} direction={['column','column','row','row']} justify='center' align='center'>
           <img src='/Clipboard05.jpg' width={'100%'} height={'100%'}/>
