6a166d232fd9f820cae874db77a83fc725633f58
mikloska <mikloska973@gmail.com>
2021-10-13 21:52:11 -0400

    Added OSLABS logo png to build instead of using link with delay.

--- public/components/Footer.jsx
+++ public/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(){
   return( 
   <div className={styles.footer}>
     <Stack direction='column'>
-      <a href="https://opensourcelabs.io/" target="_blank"  rel="noreferrer" align="center"><Image src='https://opensourcelabs.io/images/oslabs-white.png' mb={10} width={140} height={48}/></a>
+      <a href="https://opensourcelabs.io/" target="_blank"  rel="noreferrer" align="center"><Image src='/oslabs-white.png' mb={10} width={140} height={48}/></a>
       <div>Copyright © {new Date().getFullYear()} FaradayJS</div>
     </Stack>
   </div>)
